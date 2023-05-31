d84064151107900d1b462e1fd65708da0449d205
AhmedMushtaq-GitHub <ahmed@AHMEDMUSHTAQ>
2023-05-31 18:21:39 +0500

    Add project files.

---
+++ oop/Student.cs
@@ -0,0 +1,58 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace oop
+{
+    class Student
+    {
+        string fname , lname , Father_name, Mother_name;
+        int age;
+        int rollNumber;
+        int admissionDate;
+        DateOnly dateOnly;
+        string id;
+
+
+    }
+    class Custmer
+    {
+        string ful_name;
+        long phone_number;
+        DateTime purchasing_day;
+    }
+
+
+}*/
+
+/*enum wellCome
+{
+    Hello,
+    GoodBye
+}*/
+
+/*public class Calculator
+{
+    int val1;
+    int val2;
+    int result;
+
+
+    void add()
+    {
+        result = val1 + val2;
+        Console.WriteLine(result);
+
+    }
+    void sub()
+    {
+        result = val1 - val2;
+        Console.WriteLine(result);
+
+    }
+
+}
+*/
